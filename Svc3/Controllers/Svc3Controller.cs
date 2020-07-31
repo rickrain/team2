6e77648f2e1d85cf18d03dde4ab1518066997fdf
Rick Rainey <rickra@microsoft.com>
2020-07-30 19:27:28 -0500

    Initial commit.

---
+++ Svc3/Controllers/Svc3Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Svc3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello from 'Svc3'";
+        }
+    }
+}
